fe3fa9b74c4d9899c89538f63b4f97cf42869d89
HalilCan <halilcanmemoglu@gmail.com>
2017-06-03 01:13:13 +0300

    Initial commit

---
+++ app/src/main/java/com/halilcanmemoglu/geoguessr/Question.java
@@ -0,0 +1,35 @@
+package com.halilcanmemoglu.geoguessr;
+
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * Created by hcm on 24.05.2017.
+ */
+
+public class Question {
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+
+}
